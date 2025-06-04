7c83c54d9791c1c9f34502ab2aef0385494380b3
Layon Walker <layon.pedro@estudante.ufla.br>
2025-06-04 13:29:58 -0300

    melhorando design do card create

--- resources/js/pages/Tasks/Create.vue
+++ resources/js/pages/Tasks/Create.vue
@@ -3,9 +3,10 @@
 // html
 import InputError from '@/components/InputError.vue';
 
-import { Card, CardContent, CardFooter } from '@/components/ui/card';    
+import { Card, CardContent, CardFooter, CardTitle, CardDescription } from '@/components/ui/card';    
 
 import { Button } from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
@@ -19,8 +20,9 @@ import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover
 import { cn } from '@/lib/utils';
 import { DateFormatter, getLocalTimeZone } from '@internationalized/date';
 
-import { CalendarIcon } from 'lucide-vue-next';
+import { CalendarIcon, Car } from 'lucide-vue-next';
 import { useForm } from '@inertiajs/vue3';
+import CardHeader from '@/components/ui/card/CardHeader.vue';
 
 
 interface Props{
@@ -80,54 +82,74 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <form class="space-y-6" @submit.prevent="submitForm">
                 <Card class="py-6">
-                    <CardContent class="grid grid-cols-3 gap-4 space-y-6">
+                    <CardHeader>
+                        <CardTitle>Details of Task</CardTitle>
+                        <CardDescription>Fill in the details of the task you want to create.</CardDescription>
+                    </CardHeader>
 
-                        <div class="grid gap-2">
-                            <Label htmlFor="name">Task Name *</Label>
+                    <CardContent class="grid gap-6 p-6">
 
-                            <Input id="name" v-model="form.name" class="mt-1 block w-full" />
+                        <div class="grid gap-2 md:grid-cols-2 lg:grid-cols-3">
+                            <div class="md:col-span-2 lg:col-span-2 grid gap-2">
+                                <Label htmlFor="name">Task Name * <InputError :message="form.errors.name" /></Label> 
 
-                            <InputError :message="form.errors.name" />
+                                <Input id="name" v-model="form.name" class="mt-1 block w-full" />
+                                
+                            </div>
+                        
+
+                            <div class="grid gap-2">
+                                <Label htmlFor="name">Due Date</Label>
+
+                                <Popover>
+                                    <PopoverTrigger as-child>
+                                        <Button variant="outline" :class="cn('w-full justify-start text-left font-normal', !form.due_date && 'text-muted-foreground')">
+                                            <CalendarIcon class="mr-2 h-4 w-4" />
+                                            {{ form.due_date ? df.format(form.due_date.toDate(getLocalTimeZone())) : 'Select a date' }}
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent class="w-auto p-0">
+                                        <Calendar v-model="form.due_date" initial-focus />
+                                    </PopoverContent>
+
+                                    <InputError :message="form.errors.due_date" />
+                                </Popover>
+                            </div>
                         </div>
 
                         <div class="grid gap-2">
-                            <Label htmlFor="name">Due Date</Label>
+                            <Label htmlFor="is_completed">Completed</Label>
 
-                            <Popover>
-                                <PopoverTrigger as-child>
-                                    <Button variant="outline" :class="cn('w-[280px] justify-start text-left font-normal', !form.due_date && 'text-muted-foreground')">
-                                        <CalendarIcon class="mr-2 h-4 w-4" />
-                                        {{ form.due_date ? df.format(form.due_date.toDate(getLocalTimeZone())) : 'Select a date' }}
-                                    </Button>
-                                </PopoverTrigger>
-                                <PopoverContent class="w-auto p-0">
-                                    <Calendar v-model="form.due_date" initial-focus />
-                                </PopoverContent>
+                            <Switch id="is_completed" v-model:checked="form.is_completed" class="mt-1" />
 
-                                <InputError :message="form.errors.due_date" />
-                            </Popover>
+                            <InputError :message="form.errors.is_completed" />
                         </div>
 
-                        <div class="grid gap-2">
-                            <Label htmlFor="name">Media</Label>
+                        <div class="grid md:grid-cols-2 gap-6">
+                            <div class="grid gap-2">
 
-                            <Input type="file" id="name" v-on:change="fileSelected($event)" class="mt-1 block w-full" />
+                                <Label htmlFor="name">Media</Label>
 
-                            <progress v-if="form.progress" :value="form.progress.percentage" max="100">{form.progress.percentage}%</progress>
+                                <Input type="file" id="name" v-on:change="fileSelected($event)" class="w-full" />
 
-                            <InputError :message="form.errors.media" />
-                        </div>
+                                <progress v-if="form.progress" :value="form.progress.percentage" max="100" class="w-full mt-2">{form.progress.percentage}%</progress>
 
-                        <div class="grid gap-2">
-                            <Label htmlFor="categories">Categories</Label>
-        
-                            <ToggleGroup type="multiple" variant="outline" size="lg" v-model="form.categories" class="w-full">
-                                <ToggleGroupItem v-for="category in categories" :key="category.id" :value="category.id">
-                                    {{ category.name }}
-                                </ToggleGroupItem>
-                            </ToggleGroup>
-        
-                            <InputError :message="form.errors.categories" />
+                                <InputError :message="form.errors.media" />
+                                
+                            </div>
+                        
+
+                            <div class="grid gap-2">
+                                <Label htmlFor="categories">Categories</Label>
+            
+                                <ToggleGroup type="multiple" variant="outline" size="lg" v-model="form.categories" class="flex flex-wrap gap-2 justify-start w-full">
+                                    <ToggleGroupItem v-for="category in categories" :key="category.id" :value="category.id">
+                                        {{ category.name }}
+                                    </ToggleGroupItem>
+                                </ToggleGroup>
+            
+                                <InputError :message="form.errors.categories" />
+                            </div>
                         </div>
                     </CardContent>
                 
