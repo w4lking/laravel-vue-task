e54064e956fe1648755285aaf1a3985c35e705f0
Layon Walker <layon.pedro@estudante.ufla.br>
2025-06-04 12:48:23 -0300

    testando design card do create

--- resources/js/pages/Tasks/Create.vue
+++ resources/js/pages/Tasks/Create.vue
@@ -3,6 +3,8 @@
 // html
 import InputError from '@/components/InputError.vue';
 
+import { Card, CardContent, CardFooter } from '@/components/ui/card';    
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -77,57 +79,64 @@ const breadcrumbs: BreadcrumbItem[] = [
 
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <form class="space-y-6" @submit.prevent="submitForm">
-                <div class="grid gap-2">
-                    <Label htmlFor="name">Task Name *</Label>
+                <Card class="py-6">
+                    <CardContent class="grid grid-cols-3 gap-4 space-y-6">
 
-                    <Input id="name" v-model="form.name" class="mt-1 block w-full" />
+                        <div class="grid gap-2">
+                            <Label htmlFor="name">Task Name *</Label>
 
-                    <InputError :message="form.errors.name" />
-                </div>
+                            <Input id="name" v-model="form.name" class="mt-1 block w-full" />
 
-                <div class="grid gap-2">
-                    <Label htmlFor="name">Due Date</Label>
+                            <InputError :message="form.errors.name" />
+                        </div>
 
-                    <Popover>
-                        <PopoverTrigger as-child>
-                            <Button variant="outline" :class="cn('w-[280px] justify-start text-left font-normal', !form.due_date && 'text-muted-foreground')">
-                                <CalendarIcon class="mr-2 h-4 w-4" />
-                                {{ form.due_date ? df.format(form.due_date.toDate(getLocalTimeZone())) : 'Select a date' }}
-                            </Button>
-                        </PopoverTrigger>
-                        <PopoverContent class="w-auto p-0">
-                            <Calendar v-model="form.due_date" initial-focus />
-                        </PopoverContent>
+                        <div class="grid gap-2">
+                            <Label htmlFor="name">Due Date</Label>
 
-                        <InputError :message="form.errors.due_date" />
-                    </Popover>
-                </div>
+                            <Popover>
+                                <PopoverTrigger as-child>
+                                    <Button variant="outline" :class="cn('w-[280px] justify-start text-left font-normal', !form.due_date && 'text-muted-foreground')">
+                                        <CalendarIcon class="mr-2 h-4 w-4" />
+                                        {{ form.due_date ? df.format(form.due_date.toDate(getLocalTimeZone())) : 'Select a date' }}
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent class="w-auto p-0">
+                                    <Calendar v-model="form.due_date" initial-focus />
+                                </PopoverContent>
 
-                <div class="grid gap-2">
-                    <Label htmlFor="name">Media</Label>
+                                <InputError :message="form.errors.due_date" />
+                            </Popover>
+                        </div>
 
-                    <Input type="file" id="name" v-on:change="fileSelected($event)" class="mt-1 block w-full" />
+                        <div class="grid gap-2">
+                            <Label htmlFor="name">Media</Label>
 
-                    <progress v-if="form.progress" :value="form.progress.percentage" max="100">{form.progress.percentage}%</progress>
+                            <Input type="file" id="name" v-on:change="fileSelected($event)" class="mt-1 block w-full" />
 
-                    <InputError :message="form.errors.media" />
-                </div>
+                            <progress v-if="form.progress" :value="form.progress.percentage" max="100">{form.progress.percentage}%</progress>
 
-                <div class="grid gap-2">
-                    <Label htmlFor="categories">Categories</Label>
- 
-                    <ToggleGroup type="multiple" variant="outline" size="lg" v-model="form.categories">
-                        <ToggleGroupItem v-for="category in categories" :key="category.id" :value="category.id">
-                            {{ category.name }}
-                        </ToggleGroupItem>
-                    </ToggleGroup>
- 
-                    <InputError :message="form.errors.categories" />
-                </div>
+                            <InputError :message="form.errors.media" />
+                        </div>
 
-                <div class="flex items-center gap-4">
-                    <Button :disabled="form.processing" variant="default">Create Task</Button>
-                </div>
+                        <div class="grid gap-2">
+                            <Label htmlFor="categories">Categories</Label>
+        
+                            <ToggleGroup type="multiple" variant="outline" size="lg" v-model="form.categories" class="w-full">
+                                <ToggleGroupItem v-for="category in categories" :key="category.id" :value="category.id">
+                                    {{ category.name }}
+                                </ToggleGroupItem>
+                            </ToggleGroup>
+        
+                            <InputError :message="form.errors.categories" />
+                        </div>
+                    </CardContent>
+                
+                    <CardFooter>
+                        <div class="flex items-center gap-4">
+                            <Button :disabled="form.processing" variant="default">Create Task</Button>
+                        </div>
+                    </CardFooter>
+                </Card>
             </form>
         </div>
     </AppLayout>
